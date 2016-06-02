7cb05bb4ae93f4dffff56126a5d045d6e8f8cc9d
Adam Eivy <adam.eivy@disney.com>
2016-06-01 22:19:44 -0700

    now tests 2-14 combined phrases at 100 iterations to get duplicate counts

--- test/index.js
+++ test/index.js
@@ -3,8 +3,11 @@
 var assert = require('chai').assert;
 var buzzphrase = require('../');
 var exec = require('child_process').exec;
+// safe words for duplication in uniqueness tests
+var repeatables = ['by','to','of','on','for','which','in'];
 
 describe('buzzphrase', function() {
+
   describe('buzzphrase.getPhrase()', function() {
     it('will get a unique buzzphrase', function() {
         var phrase1 = buzzphrase.getPhrase();
@@ -32,34 +35,48 @@ describe('buzzphrase', function() {
         assert.notEqual(phrase1, phrase2, 'two generated phrases are different');
     });
 
-    // TODO: work out testing the gamut of options with an acceptable probability threshold for repeats
+
     it('returns reasonably unique words in combined phrases', function() {
-        var phrase = buzzphrase.getPhrase(2);
-        // strip commas and split words
-        var words = phrase.replace(/,/g, '').split(' ');
-        // pad the string phrase so we can search for " word "
-        phrase = ' ' + phrase + ' ';
-        // safe words
-        var repeatables = ['by','to','of','on','for','which','in'];
-        // exists
-        assert(phrase);
-        words.map(function(word){
-          if(repeatables.indexOf(word)!==-1){
+      // test conjoined phrases
+      // as the set grows, tolerate a little more duplication of words
+      for(var phraseLength=2; phraseLength<=14; phraseLength++){
+        var acceptable = phraseLength;
+        var duplicateCount = 0;
+        var duplicateWords = [];
+        for(var iterations=0; iterations<=100; iterations++){
+          var phrase = buzzphrase.getPhrase(phraseLength);
+          // strip commas and split words
+          var words = phrase.replace(/,/g, '').split(' ');
+          // pad the string phrase so we can search for " word "
+          phrase = ' ' + phrase + ' ';
+          words.map(function(word){
+            // ok, or already logged as a duplicate
+            if(repeatables.indexOf(word)!==-1 || duplicateWords.indexOf(word)!==-1) {
+              return word;
+            }
+            var regexp = new RegExp('('+word+')', 'g');
+            var extraAppearances = phrase.match(regexp).length-1;
+            if(extraAppearances) {
+              // add anything over 1
+              duplicateCount += extraAppearances;
+              duplicateWords.push(word);
+            }
             return word;
-          }
-          var regexp = new RegExp('('+word+')', 'g');
-          var occurances = phrase.match(regexp).length;
-          assert.equal(occurances, 1, 'word ('+word+') appears only once');
-          return word;
-        });
+          });
+        }
+        console.log('buzzphrase.getPhrase('+phraseLength+') allows', acceptable, 'duplicates in 100 iterations, found', duplicateCount);
+        assert.isBelow(duplicateCount, acceptable+1 , 'too many duplicates in ' + phraseLength + ' length phrase on "' + duplicateWords.join(',')+'"');
+      }
     });
   });
+
   describe('buzzphrase.buzz()', function() {
     it('returns nothing with buzz() (STDOUT)', function() {
       var phrase = buzzphrase.buzz();
       assert(!phrase);
     });
   });
+
   describe('global cli tool', function() {
     it('runs as a global cli', function() {
       exec('node ../', function(error, stdout, stderr) {
@@ -69,4 +86,5 @@ describe('buzzphrase', function() {
       });
     });
   });
+
 });
