abb140d881e153978b89f8bef03c9839ebc6cb4f
[._.]/ Adam Eivy <70015+atomantic@users.noreply.github.com>
2021-06-23 11:48:45 -0700

    adjust tests and nodeversions

--- test/methods/getPhrase.js
+++ test/methods/getPhrase.js
@@ -36,15 +36,16 @@ module.exports = function () {
   });
 
   it("returns reasonably unique words in combined phrases", function (done) {
-    this.timeout(3000);
+    this.timeout(5000);
     // test conjoined phrases
     // as the set grows, tolerate a little more duplication of words
     for (let phraseLength = 2; phraseLength <= 15; phraseLength++) {
       let acceptable = phraseLength - 14 > 0 ? phraseLength - 14 : 0;
       let duplicateCount = 0;
       let duplicateWords = [];
+      let phrase;
       for (let iterations = 0; iterations <= 100; iterations++) {
-        let phrase = buzzphrase.getPhrase(phraseLength);
+        phrase = buzzphrase.getPhrase(phraseLength);
         // strip commas and split words
         let words = phrase.replace(new RegExp(",", "g"), "").split(" ");
         // pad the string phrase so we can search for " word "
@@ -70,18 +71,18 @@ module.exports = function () {
           }
           return word;
         });
-        assert.isBelow(
-          duplicateCount,
-          acceptable + 1,
-          "too many duplicates in " +
-            phraseLength +
-            ' length phrase on "' +
-            duplicateWords.join(",") +
-            '"' +
-            "in phrase: " +
-            phrase
-        );
       }
+      assert.isBelow(
+        duplicateCount,
+        acceptable + 1,
+        "too many duplicates in " +
+          phraseLength +
+          ' length phrase on "' +
+          duplicateWords.join(",") +
+          '"' +
+          "in phrase: " +
+          phrase
+      );
     }
     done();
   });
